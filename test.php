032594c5981474778321fd570f80ed1818231ba1
lsat12357 <lsato@uoregon.edu>
2021-07-23 12:32:11 -0700

    make test reusable

--- test.php
+++ test.php
@@ -1,13 +1,14 @@
 <?php
+  //requires one argument, the project id, i.e. chc, pppm, or urs
   include "base_record.php";
-  include "pppm/full_record.php";
+  include "{$argv[1]}/full_record.php";
   include "specialchars.php";
   include "dublin_core_xml.php";
 
   $record = new FullRecord();
-  $record->init('pppm');
+  $record->init($argv[1]);
   $record->get_configs();
-  $lines = File("pppm/data.csv");
+  $lines = File("{$argv[1]}/data.csv");
   $arr = $record->set_metadata($lines[1]);
   var_dump($record);
   $str = $record->assemble_properties();
